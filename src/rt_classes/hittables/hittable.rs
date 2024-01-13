b00a504484b701e8467d3b3b9e8632a05d2897a4
Maximilian Falk <falk.maximilian@outlook.com>
2024-01-13 20:32:16 +0100

    Hittable List

--- src/rt_classes/hittables/hittable.rs
+++ src/rt_classes/hittables/hittable.rs
@@ -4,9 +4,29 @@ use crate::rt_classes::vec3::Vec3;
 pub struct HitRecord {
     pub pnt: Vec3,
     pub normal: Vec3,
-    pub t: f64
+    pub t: f64,
+    pub front_face: bool
+}
+
+impl HitRecord {
+    pub fn set_face_normals(& mut self, r: &Ray, outward_normal: &Vec3) {
+        // Sets the hit record normal vector.
+        // NOTE: the parameter `outward_normal` is assumed to have unit length.
+
+        self.front_face = (r.direction * outward_normal) < 0.0;
+        if self.front_face {
+            self.normal = outward_normal.clone();
+        } else {
+            self.normal = -1.0 * outward_normal.clone();
+        }
+    }
 }
 
 pub trait Hittable {
-    fn hit(r: &Ray, ray_tmin: f64, ray_tmax: f64, rec: &HitRecord) -> bool; 
+    fn hit(&self, r: &Ray, ray_tmin: f64, ray_tmax: f64, rec: &mut HitRecord) -> bool; 
+}
+
+
+pub struct HittableList {
+    pub objects: Vec<Box<dyn Hittable>>
 }
--- src/rt_classes/hittables/sphere.rs
+++ src/rt_classes/hittables/sphere.rs
@@ -1,5 +1,5 @@
 use crate::rt_classes::vec3::Vec3;
-
+use crate::rt_classes::ray::Ray;
 use super::hittable::{HitRecord ,Hittable};
 
 struct Sphere {
@@ -18,7 +18,32 @@ impl Sphere {
 
 
 impl Hittable for Sphere {
-    fn hit(r: &crate::rt_classes::ray::Ray, ray_tmin: f64, ray_tmax: f64, rec: &HitRecord) -> bool {
+    fn hit(&self, r: &Ray, ray_tmin: f64, ray_tmax: f64, rec: &mut HitRecord) -> bool {
+        let oc = r.origin - self.center;
+        let a = r.direction.length_squared();
+        let half_b = oc * r.direction;
+        let c = oc.length_squared() - self.radius*self.radius;
+
+        let discriminant = half_b*half_b - a*c;
+        if discriminant < 0.0 {
+            return false;
+        }
+
+        let sqrtd = discriminant.sqrt();
+
+        let mut root = (-half_b - sqrtd) / a;
+        if root <= ray_tmin || ray_tmax <= root {
+            root = (-half_b + sqrtd) / a;
+            if root <= ray_tmin || ray_tmax <= root {
+                return false;
+            }
+        }
+
+        rec.t = root;
+        rec.pnt = r.at(rec.t);
+        let outward_normal = (rec.pnt - &self.center) / self.radius; 
+        rec.set_face_normals(r, &outward_normal);
+        
         true
     }
 }
